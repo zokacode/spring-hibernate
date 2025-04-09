61a33894f6a20252786a2c254ee73912135762ca
z <kpro8899@protonmail.com>
2025-04-09 23:18:57 +0800

    feat: HibernateConfig modify

--- src/main/java/com/example/config/HibernateConfig.java
+++ src/main/java/com/example/config/HibernateConfig.java
@@ -8,39 +8,57 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableTransactionManagement
 public class HibernateConfig {
 
-	// 未指定name時會顯示找不到entityManagerFactory
-    @Bean(name="entityManagerFactory")
-    public LocalSessionFactoryBean sessionFactory(DataSource dataSource) {
+	private Environment env;
+
+	@Autowired
+	private void setEnv(Environment env) {
+		this.env = env;
+	}
+
+	@Bean
+    LocalSessionFactoryBean sessionFactory() {
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setDataSource(dataSource());
         sessionFactory.setPackagesToScan("com.example.model");
         sessionFactory.setHibernateProperties(hibernateProperties());
 
         return sessionFactory;
     }
+
+    @Bean
+    DataSource dataSource() {
+        // 配置數據源
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getRequiredProperty("spring.datasource.driver-class-name"));
+        dataSource.setUrl(env.getRequiredProperty("spring.datasource.url"));
+        dataSource.setUsername(env.getRequiredProperty("spring.datasource.username"));
+        dataSource.setPassword(env.getRequiredProperty("spring.datasource.password"));
+        return dataSource;
+    }
     
     private Properties hibernateProperties() {
         Properties properties = new Properties();
-        properties.put("hibernate.current_session_context_class", 
-                "org.springframework.orm.hibernate5.SpringSessionContext");
+        // properties.put("hibernate.current_session_context_class", 
+        //         "org.springframework.orm.hibernate5.SpringSessionContext");
         properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
         properties.put("hibernate.show_sql", true);
         properties.put("hibernate.format_sql", true);
-        properties.put("hibernate.hbm2ddl.auto", "update");
+        properties.put("hibernate.hbm2ddl.auto", "none");
         return properties;
     }
 
     @Bean
-    public PlatformTransactionManager hibernateTransactionManager(SessionFactory sessionFactory) {
+    PlatformTransactionManager hibernateTransactionManager(SessionFactory sessionFactory) {
     	return new HibernateTransactionManager(sessionFactory);
     }
 }
