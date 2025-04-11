ac132642eaa4e37ceb6a2ae6261e4cc8a0d95f40
z <kpro8899@protonmail.com>
2025-04-11 13:09:02 +0800

    feat: exclude JPA Config

--- src/main/java/com/example/SpringHibernateApplication.java
+++ src/main/java/com/example/SpringHibernateApplication.java
@@ -2,8 +2,13 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+	HibernateJpaAutoConfiguration.class,
+	JpaRepositoriesAutoConfiguration.class
+})
 public class SpringHibernateApplication {
 
 	public static void main(String[] args) {
